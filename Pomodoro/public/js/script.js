dce59c573bc4045d9fff7711a5a311a47c8913ae
Prasses Khadka <khadkaprasses@gmail.com>
2023-11-13 05:06:24 -0500

    .

---
+++ Pomodoro/public/js/script.js
@@ -0,0 +1,3 @@
+"use strict";
+var value = { name: "prasses", location: 7 };
+console.log(value);
---
+++ Pomodoro/src/script.ts
@@ -0,0 +1,7 @@
+interface Ivalue {
+	name: string;
+	location: number;
+}
+
+const value: Ivalue = { name: "prasses", location: 7 };
+console.log(value);
